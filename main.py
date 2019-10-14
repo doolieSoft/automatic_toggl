5bfa75cce8d234f5150b853622a5d188f25506a7
c158492 <stefano.crapanzano@chuliege.be>
2019-10-14 13:25:22 +0200

    some bugs correction

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from win32gui import GetWindowText, GetForegroundWindow
 import datetime
 import psutil, win32process, win32gui, time
 import pprint
+import os
 
 rapport = {}
 start = datetime.datetime.now()
@@ -11,9 +12,10 @@ active_app = None
 current_title = None
 
 app_logged = {}
-f_app_logged = open("app_logged.csv", "r")
+f_app_logged = open("app_logged.csv", "r", encoding="utf8")
 for line in f_app_logged:
-    app_logged[line.rstrip("\n")] = ""
+    app, project = line.split(";")
+    app_logged[app] = project.rstrip("\n")
 f_app_logged.close()
 
 
@@ -56,8 +58,11 @@ def get_titles():
         if current_title != new_title:
             current_time = datetime.datetime.now()
             time_used = current_time - start
-            print("{3} --> time used : {0:02}:{1:02}:{2:02}".format(int(time_used.seconds / 60 / 60), int(
-                time_used.seconds / 60), int(time_used.seconds), current_title))
+            hours, minutes, seconds = hours_minutes_seconds(time_used)
+            duration = "{0:02}:{1:02}:{2:02}".format(hours,
+                                                     minutes,
+                                                     seconds)
+            print("{0} --> time used : {1}".format(current_title, duration))
 
             if current_title not in rapport.keys():
                 rapport[current_title] = {}
@@ -93,8 +98,11 @@ def print_rapport(*args, **kwargs):
 
     current_time = datetime.datetime.now()
     time_used = current_time - start
-    print("{3} --> time used : {0:02}:{1:02}:{2:02}".format(int(time_used.seconds / 60 / 60), int(
-        time_used.seconds / 60), int(time_used.seconds), current_title))
+    hours, minutes, seconds = hours_minutes_seconds(time_used)
+    duration = "{0:02}:{1:02}:{2:02}".format(hours,
+                                             minutes,
+                                             seconds)
+    print("{0} --> time used : {1}".format(current_title, duration))
     # print("time used : " + str(time_used))
     print(current_title)
     if current_title not in rapport.keys():
@@ -106,34 +114,74 @@ def print_rapport(*args, **kwargs):
         rapport[current_title]['duration'] += time_used
 
     print()
+    if os.path.exists("prep_rapport-" + current_time.strftime("%Y-%m-%d") + ".csv") is True:
+        f_prep_rapport = open("prep_rapport-" + current_time.strftime("%Y-%m-%d") + ".csv", "r", encoding="utf8")
+        i = 0
+        for line in f_prep_rapport:
+            if i == 0:
+                i += 1
+                continue
+            app, email, description, project, start_date, start_time, duration = line.split(",")
+            print("Description = " + description)
+            if description not in rapport.keys():
+                rapport[description] = {}
+                rapport[description]['exe'] = app
+                rapport[description]['start_date'] = start_date
+                rapport[description]['start_time'] = start_time
+                hh, mm, ss = duration.split(":")
+                rapport[description]['duration'] = datetime.timedelta(hours=int(hh), minutes=int(mm), seconds=int(ss))
+            else:
+                print("Description in rapport.keys " + description)
+                rapport[description]['exe'] = app
+                rapport[description]['start_date'] = start_date
+                rapport[description]['start_time'] = start_time
+                hh, mm, ss = duration.split(":")
+                rapport[description]['duration'] = datetime.timedelta(hours=int(hh), minutes=int(mm), seconds=int(ss)) + \
+                                                   rapport[description]['duration']
 
-    f = open("prep_rapport.csv", "w")
-    f.write("Active app,Email,Description,Project,Start date,Start time,Duration\n")
+        f_prep_rapport.close()
+
+    f_prep_rapport = open("prep_rapport-" + current_time.strftime("%Y-%m-%d") + ".csv", "w", encoding="utf8")
+    f_prep_rapport.write("Active app,Email,Description,Project,Start date,Start time,Duration\n")
+
     print("Active app,Email,Description,Project,Start date,Start time,Duration")
     for key, value in rapport.items():
-        duration = "{0:02}:{1:02}:{2:02}".format(int(value['duration'].seconds / 60 / 60),
-                                                 int(value['duration'].seconds / 60),
-                                                 int(value['duration'].seconds))
-        f.write(value['exe'] + "," + "stefano.crapanzano@chu.ulg.ac.be" + "," + key + ",," + value['start_date'] + "," +
-                value['start_time'] + "," + duration + "\n")
-        print(value['exe'] + "," + "stefano.crapanzano@chu.ulg.ac.be" + "," + key + ",," + value['start_date'] + "," +
-              value['start_time'] + "," + duration)
+        hours, minutes, seconds = hours_minutes_seconds(value['duration'])
+        duration = "{0:02}:{1:02}:{2:02}".format(hours,
+                                                 minutes,
+                                                 seconds)
+        f_prep_rapport.write(
+            value['exe'] + "," + "stefano.crapanzano@chu.ulg.ac.be" + "," + key + "," + app_logged[value['exe']] + "," +
+            value['start_date'] + "," +
+            value['start_time'] + "," + duration + "\n")
+        print(
+            value['exe'] + "," + "stefano.crapanzano@chu.ulg.ac.be" + "," + key + "," + app_logged[value['exe']] + "," +
+            value[
+                'start_date'] + "," +
+            value['start_time'] + "," + duration)
 
+    f_prep_rapport.close()
     print()
-    f = open("rapport.csv", "w")
-    f.write("Email,Description,Project,Start date,Start time,Duration\n")
+
+    f_rapport = open("rapport-" + current_time.strftime("%Y-%m-%d") + ".csv", "w", encoding="utf8")
+    f_rapport.write("Email,Description,Project,Start date,Start time,Duration\n")
+
     print("Email,Description,Project,Start date,Start time,Duration")
     for key, value in rapport.items():
         if value['exe'] in app_logged.keys():
-            duration = "{0:02}:{1:02}:{2:02}".format(int(value['duration'].seconds / 60 / 60),
-                                                     int(value['duration'].seconds / 60),
-                                                     int(value['duration'].seconds))
-            f.write("stefano.crapanzano@chu.ulg.ac.be" + "," + key + ",," + value['start_date'] + "," +
-                    value['start_time'] + "," + duration + "\n")
-            print("stefano.crapanzano@chu.ulg.ac.be" + "," + key + ",," + value['start_date'] + "," +
+            hours, minutes, seconds = hours_minutes_seconds(value['duration'])
+            duration = "{0:02}:{1:02}:{2:02}".format(hours,
+                                                     minutes,
+                                                     seconds)
+            f_rapport.write(
+                "stefano.crapanzano@chu.ulg.ac.be" + "," + key + "," + app_logged[value['exe']] + "," + value[
+                    'start_date'] + "," +
+                value['start_time'] + "," + duration + "\n")
+            print("stefano.crapanzano@chu.ulg.ac.be" + "," + key + "," + app_logged[value['exe']] + "," + value[
+                'start_date'] + "," +
                   value['start_time'] + "," + duration)
 
-    f.close()
+    f_rapport.close()
     sys.exit(0)
 
 
@@ -149,6 +197,10 @@ def active_window_process_name():
         return psutil.Process(pid[-1]).name()  # pid[-1] is the most likely to survive last longer
     except:
         return None
+
+
+def hours_minutes_seconds(td):
+    return td.seconds // 3600, (td.seconds // 60) % 60, td.seconds % 60
 
 
 if __name__ == '__main__':
